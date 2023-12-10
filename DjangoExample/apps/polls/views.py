bc8b8728cbabf49ca5143ac2e148ab8de0e2fd13
Eric Bergerson <eb@object.com>
2023-12-09 22:02:54 -0500

    Initial Commit

---
+++ DjangoExample/apps/polls/views.py
@@ -0,0 +1,27 @@
+import uuid
+
+from django.http import HttpResponse, HttpRequest, Http404
+from django.shortcuts import render, get_object_or_404
+
+from .models import Question
+
+
+def index(request: HttpRequest) -> HttpResponse:
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    context = {"latest_question_list": latest_question_list}
+    return render(request, "polls/index.html", context)    # uid = uuid.uuid1()
+    # return HttpResponse(f"Hello, world.  polls: {uid}")
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
+    # return HttpResponse("You're looking at question %s." % question_id)
+
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
